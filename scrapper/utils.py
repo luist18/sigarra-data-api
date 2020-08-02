ade27b4d57fc2d33a5ff53f24c3249b6922b7fc8
Luís Tavares <luistavares10@outlook.pt>
2020-08-02 23:32:44 +0100

    Add encoding to files

--- scrapper/utils.py
+++ scrapper/utils.py
@@ -1,4 +1,6 @@
-import json
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 SIGARRA_URLS = {
     'homepage': 'https://sigarra.up.pt/feup/pt/',
     'course_plan': 'https://sigarra.up.pt/feup/pt/cur_geral.cur_planos_estudos_view?pv_plano_id={}&pv_ano_lectivo=2019&pv_tipo_cur_sigla=MI&pv_origem=CUR',
